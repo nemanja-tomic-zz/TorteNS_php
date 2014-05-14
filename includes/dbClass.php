1510fb25a1b804002ae04794c3a93714e35b413e
nemanja-tomic <t.nemanja@hotmail.com>
2014-05-15 01:11:09 +0200

    Added configmanager.class.php. Implemented AutoLoader logic.

--- includes/dbClass.php
+++ includes/dbClass.php
@@ -4,15 +4,15 @@
 class Database
 {
 	private $server = "localhost:3306";
-	private $username = "atila";
-	private $password = "penis";
+	private $username = "root";
+	private $password = "root";
 	public $con = null;
 	
 	public function connect()
 	{
 			if (!$con = mysql_connect($this->server, $this->username, $this->password))
 				throw new Exception("Neuspela konekcija sa bazom podataka: ".mysql_error());
-			if(!mysql_select_db("TorteDB", $con))
+			if(!mysql_select_db("tortedb", $con))
 				throw new Exception("Ne postoji birana baza podataka na serveru.");
 			$this->con = $con;
 			return $this->con;
