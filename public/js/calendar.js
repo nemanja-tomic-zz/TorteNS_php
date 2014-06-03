1c74a8e932b7cd221206872b30934f1e010ac66c
ayperosss <batila.it@gmail.com>
2014-06-03 22:14:21 +0200

    linked calendar with php. PHP date subtract is missing

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,104 +1,54 @@
 $(document).ready(function () {
-    var d = new Date();
-    window.currentDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+var ordersList = [];
 
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            //right: 'month,agendaWeek,agendaDay'
-            right: 'month,agendaWeek'
-        },
-        defaultDate: window.currentDate,
-        firstDay: 1,
-        editable: false,
-        events: [
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-04'
-            },
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-01'
-            },
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-07'
-            },
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-08'
-            },
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-01'
-            },
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-01'
-            },
-            {
-                title: 'Long Event',
-                name: 'ol dej ivent',
-                start: '2014-05-07',
-                end: '2014-05-10'
-            },
-            {
-                title: 'All Day Event',
-                name: 'ol dej ivent',
-                start: '2014-05-01'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2014-04-09T16:00:00',
-                name: 'ol dej ivent'
-            },
-            {
-                id: 999,
-                name: 'ol dej ivent',
-                title: 'Repeating Event',
-                start: '2014-05-16T16:00:00'
-            },
-            {
-                title: 'Meeting',
-                name: 'ol dej ivent',
-                start: '2014-04-12T10:30:00',
-                end: '2014-01-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                name: 'ol dej ivent',
-                start: '2014-01-12T12:00:00'
-            },
-            {
-                title: 'Birthday Party',
-                name: 'ol dej ivent',
-                start: '2014-01-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                name: 'ol dej ivent',
-                url: 'http://google.com/',
-                start: '2014-01-28'
+    var data= {ime : "", naziv:"", cena:"", napomena:""};
+    $.post("includes/api.php", {action: "filterorders", data: JSON.stringify(data)}, function(json){
+        var obj = $.parseJSON(json);
+        for (var i= 0; i<obj.data.length; i++) {
+            //setuj pocetni dan - od datuma transakcije oduzeti dane spremanja
+            var datumTransakcije = new Date (obj.data[i].datumTransakcije);
+            var imeklase = "calendarClass"+(obj.data[i].idGrupe);
+
+            var newObject = {
+                id : obj.data[i].id,
+                title: obj.data[i].naziv,
+                start: datumTransakcije,
+                end: datumTransakcije,
+                className : imeklase
             }
-        ],
-        eventClick: function(event) {
-            console.log(event);
-        },
-        dayClick: function() {
-            alert('a day has been clicked!');
-        },
-        eventMouseOver:function() {
-
+            ordersList.push(newObject);
+            newObject = null;
         }
+
+        var d = new Date();
+        window.currentDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev today',
+                center: 'title',
+                //right: 'month,agendaWeek,agendaDay'
+                right: 'today next'
+            },
+            defaultDate: window.currentDate,
+            firstDay: 1,
+            editable: false,
+            events: ordersList,
+            eventClick: function(event) {
+                console.log(event);
+            },
+            dayClick: function() {
+                alert('a day has been clicked!');
+            },
+            eventMouseOver:function() {
+
+            }
+        });
+        $('#calendar').fullCalendar('today');
+
     });
-    $('#calendar').fullCalendar('today');
+
+
+
 });
 
